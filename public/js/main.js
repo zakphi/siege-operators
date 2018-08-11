55b2369352a2e2f20d9da7d3dc5d770ffe5b7b95
Philip Zak <zakphi@gmail.com>
2018-08-11 02:10:58 -0400

    create app skeleton

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,20 @@
 import operators from '../data/operators.js'
 
 console.log(operators)
+
+function createAppSkeleton(){
+  let container = document.createElement('div')
+  container.id = 'container'
+
+  let filterControls = document.createElement('div')
+  filterControls.id = 'filterControls'
+
+  let operatorsContainer = document.createElement('div')
+  operatorsContainer.id = 'operators'
+
+  container.appendChild(filterControls)
+  container.appendChild(operatorsContainer)
+  document.body.appendChild(container)
+}
+
+createAppSkeleton()
