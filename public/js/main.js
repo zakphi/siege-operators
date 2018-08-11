3ec6ee5e891e5de9ba6f8ebb47866de1e4607ba6
Philip Zak <zakphi@gmail.com>
2018-08-11 02:19:10 -0400

    render filter controls

--- public/js/main.js
+++ public/js/main.js
@@ -17,4 +17,32 @@ function createAppSkeleton(){
   document.body.appendChild(container)
 }
 
+function renderFilterControls(){
+  let atkBtn = document.createElement('button')
+  atkBtn.id = 'atkBtn'
+  atkBtn.innerText = 'attackers'
+
+  let defBtn = document.createElement('button')
+  defBtn.id = 'defBtn'
+  defBtn.innerText = 'defenders'
+
+  let unitsDropdown = document.createElement('select')
+
+  let uniqueUnits = [...new Set(operators.map(operator => operator.unit))]
+  console.log(uniqueUnits)
+  uniqueUnits.map(uniqueUnit => {
+    let unitOption = document.createElement('option')
+    unitOption.value = uniqueUnit
+    unitOption.text = uniqueUnit
+    unitsDropdown.appendChild(unitOption)
+  })
+
+  let container = document.querySelector('#container')
+
+  container.appendChild(atkBtn)
+  container.appendChild(defBtn)
+  container.appendChild(unitsDropdown)
+}
+
 createAppSkeleton()
+renderFilterControls()
